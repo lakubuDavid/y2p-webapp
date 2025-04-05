4dea06b9d7252cd2bd26961967e92a66afff77ce
lakubuDavid <Lakubudavid@gmail.com>
2025-04-05 12:51:38 +0400

    update: no toast on some errors

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,32 +1,34 @@
 import { defineStore } from "pinia";
 import { api } from "../../lib/client";
 import type { ApiResponse, UserData } from "../../lib/types";
-import { useToast } from "primevue/usetoast";
+// import { useToast } from "primevue/usetoast";
 import { ref } from "vue";
 
 export const useUserStore = defineStore("user", () => {
-  const toast = useToast();
+  // const toast = useToast();
   const user = ref<UserData>();
   const error = ref<Error>();
 
   const fetchUser = async () => {
     const { data, ok, originalError } = await api.get("/user/me");
     if (!ok) {
-      toast.add({
-        severity: "error",
-        summary: "Cannot fetch userr info",
-        detail: originalError.message,
-      });
+      // toast.add({
+      //   severity: "error",
+      //   summary: "Cannot fetch userr info",
+      //   detail: originalError.message,
+      // });
+      console.log(originalError)
       error.value = originalError
       return;
     }
     const { data: userData, error: _error } = data as ApiResponse<UserData>;
     if (_error) {
-      toast.add({
-        severity: "error",
-        detail: _error.message,
-        summary: "Cannot fetch user info",
-      });
+      // toast.add({
+      //   severity: "error",
+      //   detail: _error.message,
+      //   summary: "Cannot fetch user info",
+      // });
+      console.log(_error)
     }
     error.value = _error;
 
